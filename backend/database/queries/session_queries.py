17a36979038a3b9a60b8ada643822a5fdf22270a
Emine Noor <114260503+eminenoor@users.noreply.github.com>
2025-01-03 16:59:45 +0300

    lane

--- backend/database/queries/session_queries.py
+++ backend/database/queries/session_queries.py
@@ -12,7 +12,7 @@ SELECT DISTINCT s.session_id, s.date, s.start_time, s.end_time
 FROM session s
 JOIN lane l ON l.pool_id = %s  -- Use pool_id passed as a parameter
 LEFT JOIN booking b ON b.session_id = s.session_id AND b.lane_number = l.lane_number
-WHERE b.booking_id IS NULL
+WHERE b.booking_id IS NULL AND s.date != '0000-00-00 00:00:00';
 ORDER BY s.date, s.start_time;
 """
 
